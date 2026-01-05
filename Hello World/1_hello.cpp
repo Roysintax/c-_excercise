c3caac72651d1e222c1ca27b01acf7df793e9be4
Roysintax <roysihan208@gmail.com>
2026-01-05 21:08:19 +0700

    confirm

---
+++ Hello World/1_hello.cpp
@@ -0,0 +1,32 @@
+/*
+"Welcome to the first chapter of the C++ course! 🚀
+
+The programming language we are learning is called C++ (pronounced “C plus plus”), created by a Danish developer named Bjarne Stroustrup in 1979.
+
+Because it’s an improvement to the C language, it was originally named “C with Classes”.
+
+Today, it’s one of the most famous programming languages because of its incredible performance, control, and reliability. It's found in:
+
+- Operating Systems
+- Game Development
+- Robotics & Hardware
+- And many more!
+
+It’s what’s used to build operating systems like Windows and Linux, web browsers like Google Chrome and Safari, games like Fornite and Grand Theft Auto, software for SpaceX rockets and Bitcoin, and heck, it’s even the language of choice for microwaves!
+
+Not only that, it’s still taught in schools around the world because of its memory management, object-oriented programming, and the fundamentals of how computer systems work.
+
+The code we write will be in C++ files, with .cpp extension. And we write them in a code editor.
+
+A code editor is a text editor where we can write and execute code.
+
+Let’s give it a try!"
+*/
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}       
