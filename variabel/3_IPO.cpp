6328ccc343695b1560560113a93e236ac465b159
Roysintax <roysihan208@gmail.com>
2026-01-06 23:14:20 +0700

    confirm

---
+++ variabel/3_IPO.cpp
@@ -0,0 +1,62 @@
+//#include <iostream>
+
+//int main() {
+    
+    // After we declare a variable, then we can give it a value to store!
+    // Let’s use the score variable one more time.
+    //int score;
+
+    //score = 0;
+
+    // score is the variable name.
+    // = means assignment.
+    // ; to end a statement.
+
+    // In C++, an = equal sign doesn’t really mean “equal”. It’s more of a verb… it means “assign”. 
+    // Here, we are assigning the score variable a value of 0.
+    
+    // And you can update the value however many times you want later in the program:
+    
+    // int score; (Variable already declared above)
+
+    //score = 0;
+    //score = 10;
+    //score = 8;
+    //score = 10;
+
+    //std::cout << score;   // Output: 10
+
+    // Notice how here, we didn't put double quotes around the score variable. 
+    // This is because we can print the value of a variable by putting the variable name after the <<.
+    
+    // One more thing! We can combine the declaration and the initialization of a variable into one line. 
+    // So the code above can be:
+    
+    // int score = 0; (Commented out because 'score' is already declared)
+
+    //score = 10;
+    //score = 8;
+    //score = 10;
+
+    // Note: A variables only needs to be declared once.
+    // Let’s try it out!
+
+  //  return 0;
+//}
+
+#include <iostream>
+
+int main() {
+  // Write code here 💖
+  double reddit;
+
+  reddit = 10.22;
+  reddit = 80.22;
+  reddit = 29.22;
+  reddit = 12.22;
+  reddit = 65.22;
+  reddit = 844.22;
+
+  std::cout << reddit;
+  
+}
